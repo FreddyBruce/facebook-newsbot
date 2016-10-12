52a8faafedfc2b7f330be3ae771d01bab0523cab
FreddyBruce <freddy.bruce@chch.ox.ac.uk>
2016-10-12 19:52:23 +0100

    Adding webhook

--- newsbot/routes/index.js
+++ newsbot/routes/index.js
@@ -6,4 +6,28 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 'hello_token_success') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
+
+router.post('/webhook/', function(req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++ ) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      // Handle a text message from this sender
+    }
+  }
+  res.sendStatus(200);
+});
+
 module.exports = router;
