b1079e99d5c62edd1ad128890b2c287f375af028
FreddyBruce <freddy.bruce@chch.ox.ac.uk>
2016-10-16 08:11:41 +0100

    Recieving and sending messages from facebook

--- newsbot/routes/index.js
+++ newsbot/routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var request = require('request');
+var rssReader = require('feed-read');
 var router = express.Router();
 
 /* GET home page. */
@@ -17,17 +19,151 @@ router.get('/webhook', function(req, res) {
   }
 });
 
-router.post('/webhook/', function(req, res) {
-  messaging_events = req.body.entry[0].messaging;
-  for (i = 0; i < messaging_events.length; i++ ) {
-    event = req.body.entry[0].messaging[i];
-    sender = event.sender.id;
-    if (event.message && event.message.text) {
-      text = event.message.text;
-      // Handle a text message from this sender
-    }
-  }
-  res.sendStatus(200);
+router.post('/webhook', function (req, res) {
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object == 'page') {
+    // Iterate over each entry
+    // There may be multiple if batched
+    data.entry.forEach(function(pageEntry) {
+      var pageID = pageEntry.id;
+      var timeOfEvent = pageEntry.time;
+
+      // Iterate over each messaging event
+      pageEntry.messaging.forEach(function(messagingEvent) {
+        if (messagingEvent.optin) {
+          receivedAuthentication(messagingEvent);
+        } else if (messagingEvent.message) {
+          receivedMessage(messagingEvent);
+        } else if (messagingEvent.delivery) {
+          receivedDeliveryConfirmation(messagingEvent);
+        } else if (messagingEvent.postback) {
+          receivedPostback(messagingEvent);
+        } else {
+          console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+        }
+      });
+    });
+    // Assume all went well.
+//
+// You must send back a 200, within 20 seconds, to let us know you've
+// successfully received the callback. Otherwise, the request will time out.
+res.sendStatus(200);
+}
 });
+
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:",
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  // You may get a text or attachment but not both
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+
+    // If we receive a text message, check to see if it matches any special
+    // keywords and send back the corresponding example. Otherwise, just echo
+    // the text we received.
+    switch (messageText) {
+      case 'image':
+        sendImageMessage(senderID);
+        break;
+
+      case 'button':
+        sendButtonMessage(senderID);
+        break;
+
+      case 'generic':
+        sendGenericMessage(senderID);
+        break;
+
+      case 'receipt':
+        sendReceiptMessage(senderID);
+        break;
+
+      default:
+        getArticles(function(err, articles) {
+          sendTextMessage(senderID, articles[0]);
+        });
+
+
+    }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+
+var googleNewsEndpoint = "https://news.google.com/news?output=rss";
+
+function getArticles(callback) {
+  rssReader(googleNewsEndpoint, function(err, articles) {
+    if (err) {
+      callback(err);
+    } else {
+      if (articles.length > 0) {
+        callback(null, articles);
+      } else {
+        callback("no articles recieved");
+      }
+    }
+  });
+}
+
+function sendTextMessage(recipientId, article) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type:"template",
+        payload: {
+          template_type:"generic",
+          elements: [
+            {
+              title: article.title,
+              subtitle: article.published.toString(),
+              item_url: article.link
+            }
+          ]
+        }
+      }
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: 'EAAMpQnSc4ogBANwG7EnNADfYztyr9QjH1baREwEWlstwZBreA2i3gPcOwHtMzVvNA1LrZAGJBMMc0aIc9s3vcBAaHRZA9QlsnzhpOpvcZALsK1WmERB6NJYYLnzWMpPLZBrZAJbHMcikdkNABL048Pj29tWeLq7d2fiwMx2aKZBmgZDZD' },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s",
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });
+}
 
 module.exports = router;
